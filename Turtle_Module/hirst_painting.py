57e44ce928600f6eec9c83cf2ce75803d7a0cdf4
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-01-14 00:01:41 +0100

    Create hirst_painting.py

---
+++ Turtle_Module/hirst_painting.py
@@ -0,0 +1,3 @@
+# Documentation colorgram package: https://pypi.org/project/colorgram.py/
+# Hirst Spot painting: https://cdn.cnn.com/cnnnext/dam/assets/200430102527-01-damien-hirst-severed-spots-full-169.jpg
+

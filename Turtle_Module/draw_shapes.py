391a56277efb985a4edd2db0d924485ef4c76404
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-01-13 21:02:21 +0100

    Update draw_shapes.py

--- Turtle_Module/draw_shapes.py
+++ Turtle_Module/draw_shapes.py
@@ -3,9 +3,9 @@ from turtle import Turtle, Screen
 tim = Turtle()
 
 tim.shape("arrow")
-tim.color("tomato4")
 
 # Triangle Shape
+tim.color("tomato4")
 tim.forward(100)
 tim.right(120)
 tim.forward(100)
@@ -132,6 +132,35 @@ tim.right(36)
 tim.forward(100)
 tim.right(36)
 
+
+screen = Screen()
+screen.exitonclick()
+
+
+##### ALTERNATIVELY #####
+
+from turtle import Turtle, Screen
+import random
+
+# we can import it this way also
+# from random import Random
+# random = Random()
+
+tim = Turtle()
+
+tim.shape("arrow")
+
+colors = ["tomato4", "chartreuse3", "DarkSlateGrey", "DeepPink", "DarkOrange", "aquamarine4", "bisque4", "purple"]
+
+def draw_shape(num_sides):
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()

1124ab7e9622690787f8248ef3c8f40c8c04b3d7
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-01-14 01:09:55 +0100

    Update hirst_painting.py

--- Turtle_Module/hirst_painting.py
+++ Turtle_Module/hirst_painting.py
@@ -1,3 +1,4 @@
+# About Hirst Paintings: https://www.artsy.net/artist-series/damien-hirst-spots
 # Documentation colorgram package: https://pypi.org/project/colorgram.py/
 # Hirst Spot painting: https://cdn.cnn.com/cnnnext/dam/assets/200430102527-01-damien-hirst-severed-spots-full-169.jpg
 
@@ -36,3 +37,44 @@ of this project. Therefore, the first aim has been achieved successfully, which 
  (199, 14, 10), (243, 33, 165), (229, 17, 121), (73, 9, 31), (60, 14, 8), (224, 141, 211), (10, 97, 61), (221, 160, 9), (17, 18, 43), (46, 214, 232), (11, 227, 239), 
  (81, 73, 214), (238, 156, 220), (74, 213, 167), (77, 234, 202), (52, 234, 243), (3, 67, 40)]
  '''
+
+
+
+
+
+#### THE REAL GOAL OF THE PROJECT IS BELOW #### HIRST PAINTINGS
+
+import turtle
+from turtle import Turtle, Screen
+import random
+
+turtle.colormode(255)
+tim = Turtle()
+tim.speed("fastest")
+tim.penup()  # this helps in removing the turtle lines
+tim.hideturtle()  # same as using the shortened .ht()
+
+color_list = [(232, 251, 242), (198, 12, 32), (250, 237, 17), (39, 76, 189), (38, 217, 68), (238, 227, 5), (229, 159, 46), (27, 40, 157), (215, 74, 12), (15, 154, 16), (199, 14, 10), (243, 33, 165), (229, 17, 121), (73, 9, 31), (60, 14, 8), (224, 141, 211), (10, 97, 61), (221, 160, 9), (17, 18, 43), (46, 214, 232), (11, 227, 239), (81, 73, 214), (238, 156, 220), (74, 213, 167), (77, 234, 202), (52, 234, 243), (3, 67, 40)]
+
+# Let's tweak the position of where the dot should start from in our turtle GUI
+
+tim.setheading(225)  # best positions seems to be within 270 - 360, then I chose to place it at their middle
+tim.forward(300)   # we counted this in 50s increments (can be any number like 250), for the dots in our for loop below
+tim.setheading(0)  # in order to make takes its new default position in the tweaked position
+
+for dot_count in range(1, 101):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        # Now, we want to turn turtle left, with a forward gap of 50, and then turn left again
+        tim.left(90)  # is same as this tim.setheading(90)
+        tim.forward(50)
+        tim.left(90)  # is same as this tim.setheading(180)
+        # this below moves our turtle immediately back to the opposite side
+        # the forward value is determined by the number of forward dots (which one is 50) present
+        tim.forward(500)
+        tim.setheading(0)  # sets the turtle head to face right immediately
+
+screen = Screen()
+screen.exitonclick()
